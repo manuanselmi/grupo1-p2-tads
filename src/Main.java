42da92c52580930ea878a1fde3ae39e1ac169c04
manua <manuanselmiporras@gmail.com>
2023-06-06 12:53:47 -0300

    Tads prontos para entregar

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
-import entities.SistemaCentral;
-import com.opencsv.exceptions.CsvValidationException;;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.Scanner;
-
 public class Main {
-
-
     public static void main(String[] args) {
-
+        System.out.println("Hello world!");
+    }
 }
